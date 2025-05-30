dc33e9574849b774cd355a1e7b6111288a2ae495
Jiya-Rathi <jiyarathi1402@gmail.com>
2025-05-30 03:26:07 -0700

    Cleaned up boilerplate and initialized project

---
+++ progress-tracker/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Progress Tracker</h1>
+      <p>Welcome! This will track your coding, thesis, studies, and more.</p>
+    </div>
+  );
+}
+
+export default App;
